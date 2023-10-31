03efb0c2b5d7fd7706a23580110b4e5636e8afd5
Tyuiu.ZakharovaYV@outlook.com <justik4433@gmail.com>
2023-10-31 12:11:23 +0500

    Add project files.

---
+++ Tyuiu.ZakharovaYV.Sprint3.Review.V2.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ZakharovaYV.Sprint3.Review.V2.Lib
+{
+    public class DataService
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round((Math.Cos(x) / (x - 0.7)) - Math.Sin(x) * 12 * x + 2, 2);
+                valueArray[count] = y;
+                count++;
+            }
+            return valueArray;
+
+        }
+    }
+}
---
+++ Tyuiu.ZakharovaYV.Sprint3.Review.V2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZakharovaYV.Sprint3.Review.V2.Lib;
+
+namespace Tyuiu.ZakharovaYV.Sprint3.Review.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт#3 |Выполнила: Захарова Ю.В. |ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#3                                                                *");
+            Console.WriteLine("* Тема: Итоговое решение по спринту                                       *");
+            Console.WriteLine("* Задание # Review                                                        *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнила: Захарова Юлиана Владимировна | ПКТб-23-2                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу котрая выводит таблицу значений функций              *");
+            Console.WriteLine("*   (cos(x)/x-0.7) - sin(x) * 12x + 2                                     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int startValue = -5;
+            int stopValue = 5;
+
+
+
+            Console.WriteLine("Старт шага  =" + startValue);
+            Console.WriteLine("Конец шага  =" + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    x     |   f(x)   |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("| {0,7:d}  |  {1,6:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+        }
+
+    }
+}
+
